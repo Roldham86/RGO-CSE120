32c1eb64101bca0a66324a34f893c881f09a81aa
Roldham86 <old19001@byui.edu>
2025-01-11 15:09:25 -0700

    prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,15 @@
-using System;
-
+using System; 
+//made by w00f
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name?");
+        string First = Console.ReadLine();
+        Console.Write("What is your Last name?");
+        string Last = Console.ReadLine();
+
+        //Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine($"Your name is {Last}, {First} {Last}.");
     }
 }
